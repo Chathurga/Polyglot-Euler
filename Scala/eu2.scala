a98623266f292da2fe48af30f5fae4a76cf7e81a
Chathurga <chathurga@gmail.com>
2014-09-18 14:12:03 +0100

    Add Euler 2 - Scala

---
+++ Scala/eu2.scala
@@ -0,0 +1,11 @@
+// Scala has the ability to produce lazy sequences using Stream
+
+object Main {
+  def main(args: Array[String]) {
+    println(fib(0, 1) takeWhile {_ < 4000000} filter {_ % 2 == 0} sum)
+  }
+  
+  def fib(x1: Int, x2: Int): Stream[Int] = {
+    x1 #:: fib(x2, x1 + x2)
+  }
+}
