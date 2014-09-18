ad4a040b268991817d9469dda8a1117d5abda15b
Conor McManus <chathurga@gmail.com>
2014-09-18 11:27:33 +0100

    Euler 1 - Scala: Added comments

--- Scala/eu1.scala
+++ Scala/eu1.scala
@@ -1,8 +1,12 @@
 // Scala's parsing rules can be strange at first
-// 1 to 100 is actually the call (1).to(100)
-// If the expression is a value then a space is sugar for the . operator
-// If the expression is a function expecting 1 parameter then a space is
-//   sugar for function application
+// Notes:
+//  * Functions that don't take arguments do not need parenthesis
+//  * 1 to 100 is actually the call (1).to(100)
+//  * If the expression is a value then a space is sugar for the . operator
+//  * If the expression is a function expecting 1 parameter then a space is
+//    sugar for function application
+//  * Because Scala is statically typed it knows how to properly desugar
+//    spaces without ambiguity
 object Main {
   def main(args: Array[String]) {
     println(1 to 999 filter {x: Int => x % 3 == 0 || x % 5 == 0} sum)
