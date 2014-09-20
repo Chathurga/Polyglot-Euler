a08da5fe5d28e8da7af88d86834dc469d9fc810f
Conor McManus <chathurga@gmail.com>
2014-09-20 23:26:36 +0100

    Added: Euler 2 - SQL

---
+++ SQL/eu002.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION fib_to_4mil() RETURNS SETOF integer
+AS $$
+DECLARE x1 integer;
+DECLARE x2 integer;
+DECLARE x3 integer;
+BEGIN
+
+SELECT 0 INTO x1;
+SELECT 1 INTO x2;
+
+<<counter>>
+LOOP
+    IF x2 < 4000000 THEN
+        RETURN NEXT x2;
+        SELECT (x1 + x2) INTO x3;
+        SELECT x2 INTO x1;
+        SELECT x3 INTO x2;
+    ELSE
+        EXIT counter;
+    END IF;
+END LOOP;
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT SUM(fib) FROM fib_to_4mil() as fib WHERE fib % 2 = 0;
