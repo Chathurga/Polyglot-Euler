eefa227a2f39acfa8b191cb52fe0d03ea677de21
Conor McManus <chathurga@gmail.com>
2014-09-21 11:58:34 +0100

    Changed: Euler 5 - Prolog, minor cleanups

--- Prolog/eu005.pl
+++ Prolog/eu005.pl
@@ -1,10 +1,4 @@
-:- use_module(library(aggregate)).
 :- initialization(search).
-
-lcm(X, Y, Mult) :-
-    XY is X * Y,
-    Div is gcd(X, Y),
-    Mult is XY / Div.
 
 search :-
     search(19, 20, Mult),
@@ -15,3 +9,8 @@ search(N, Acc, Mult) :-
     lcm(N, Acc, Acc2),
     N2 is N - 1,
     search(N2, Acc2, Mult).
+
+lcm(X, Y, Mult) :-
+    XY is X * Y,
+    Div is gcd(X, Y),
+    Mult is XY / Div.
