09f048307bca345b930f6953ebc857718635ee5e
Conor McManus <chathurga@gmail.com>
2014-09-21 01:31:52 +0100

    Added: Euler 2 - C

---
+++ C/eu002.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int x1 = 0, x2 = 1, x3, sum = 0;
+
+    while (x2 < 4000000) {
+        if (x2 % 2 == 0) {
+            sum += x2;
+        }
+        x3 = x1 + x2;
+        x1 = x2;
+        x2 = x3;
+    }
+
+    printf("%d\n", sum);
+}
