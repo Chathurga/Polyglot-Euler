9a2b9c22861468cd612052b5ad543a81c57471e0
Conor McManus <chathurga@gmail.com>
2014-09-21 03:02:28 +0100

    Rewrote: Euler 3 - Prolog

--- Prolog/eu003.pl
+++ Prolog/eu003.pl
@@ -1,26 +1,22 @@
-search(Result) :- largest_prime_factor(600851475143, Result).
+:- initialization search.
 
-largest_prime_factor(X, Factor) :-
-    Root is floor(sqrt(X)),
-    % Putting in Root instead of 10000 causes stack overflow, not sure why
-    primes_to(10000, Primes),
-    reverse(Primes, RevPrimes),
-    first_factor(X, RevPrimes, Factor).
+search :-
+    largest_prime_factor(600851475143, Result),
+    print(Result), nl, halt.
 
-first_factor(X, [Factor|_], Factor) :- 0 is X mod Factor.
-first_factor(X, [_|List], Factor) :- first_factor(X, List, Factor).
+largest_prime_factor(X, Factor) :- largest_prime_factor(X, 3, [2], Factor).
+largest_prime_factor(P, P, Primes, P) :- no_factors(P, Primes).
+largest_prime_factor(X, P, Primes, Factor) :-
+    no_factors(P, Primes),
+    reduce(X, P, X2),
+    P2 is P + 2,
+    largest_prime_factor(X2, P2, [P|Primes], Factor).
+largest_prime_factor(X, P, Primes, Factor) :-
+    P2 is P + 2,
+    largest_prime_factor(X, P2, Primes, Factor).
 
-primes_to(Limit, Primes) :- primes_to(3, Limit, [2], Primes).
-primes_to(X, Limit, Acc, Acc) :- X > Limit.
-primes_to(X, Limit, Acc, Primes) :-
-    trial_div(X, Acc, Acc, NewAcc),
-    Y is X + 2,
-    primes_to(Y, Limit, NewAcc, Primes).
+no_factors(_, []).
+no_factors(X, [H|T]) :- 0 =\= X mod H, no_factors(X, T).
 
-trial_div(X, Acc, [], NewAcc) :- append(Acc, [X], NewAcc).
-trial_div(X, Acc, [P|_], Acc) :- 0 is X mod P.
-trial_div(X, Acc, [_|PS], NewAcc) :- trial_div(X, Acc, PS, NewAcc).
-
-reverse(List, Rev) :- reverse(List, [], Rev).
-reverse([], Rev, Rev).
-reverse([H|T], Acc, Rev) :- reverse(T, [H|Acc], Rev).
+reduce(X, Y, X2) :- 0 is X mod Y, X2 is X / Y.
+reduce(X, _, X).
