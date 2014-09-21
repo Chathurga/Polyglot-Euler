4f828214d0b9618c2685d3011c8754885eb806a4
Conor McManus <chathurga@gmail.com>
2014-09-21 22:49:51 +0100

    Rewrote: Euler 3 - Bash, greatly simplified by using sed

--- Bash/eu003.sh
+++ Bash/eu003.sh
@@ -1,4 +1,5 @@
 # Using the handy factor command (GNU coreutils) makes this problem easy
+# factor returns a space seperated string, largest prime at the end
+# Use sed to match the last number on the line and extract it
 
-read -a factors <<< $(factor 600851475143);
-echo ${factors[${#factors[@]}-1]};
+factor 600851475143 | sed -r "s/.* ([0-9]+)$/\1/";
