bbcda517cad736397735ec3fedaa12f9fa4b3f64
Conor McManus <chathurga@gmail.com>
2014-09-21 18:07:33 +0100

    Commented: Euler 1 - Bash

--- Bash/eu001.sh
+++ Bash/eu001.sh
@@ -1,3 +1,7 @@
+# Bash is hilarious, reverse if (fi) closes if statement but done closes for?
+# Very useful to know bash and its ecosystem (standard *nix commands) even
+#   though I'm not a fan of the language
+
 sum=0;
 for i in {1..999}; do
     if [[ $((i % 3)) == 0 || $((i % 5)) == 0 ]]; then
