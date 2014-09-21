60c20a1a923bee6eac6160e6a5c24b3740f59316
Conor McManus <chathurga@gmail.com>
2014-09-21 10:41:08 +0100

    Changed: Euler 3 - Prolog, minor cleanups

--- Prolog/eu003.pl
+++ Prolog/eu003.pl
@@ -1,19 +1,19 @@
 :- initialization search.
 
 search :-
-    largest_prime_factor(600851475143, Result),
+    max_prime_factor(600851475143, Result),
     print(Result), nl, halt.
 
-largest_prime_factor(X, Factor) :- largest_prime_factor(X, 3, [2], Factor).
-largest_prime_factor(P, P, Primes, P) :- no_factors(P, Primes).
-largest_prime_factor(X, P, Primes, Factor) :-
+max_prime_factor(X, Factor) :- max_prime_factor(X, 3, [2], Factor).
+max_prime_factor(P, P, _, P).
+max_prime_factor(X, P, Primes, Factor) :-
     no_factors(P, Primes),
     reduce(X, P, X2),
     P2 is P + 2,
-    largest_prime_factor(X2, P2, [P|Primes], Factor).
-largest_prime_factor(X, P, Primes, Factor) :-
+    max_prime_factor(X2, P2, [P|Primes], Factor).
+max_prime_factor(X, P, Primes, Factor) :-
     P2 is P + 2,
-    largest_prime_factor(X, P2, Primes, Factor).
+    max_prime_factor(X, P2, Primes, Factor).
 
 no_factors(_, []).
 no_factors(X, [H|T]) :- 0 =\= X mod H, no_factors(X, T).
