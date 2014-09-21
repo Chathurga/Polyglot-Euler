3c451d5984fb9dfc74e61c686c4bc7d214e86fdd
Conor McManus <chathurga@gmail.com>
2014-09-21 23:17:02 +0100

    Improved: Euler 2 - Prolog, now runs as standalone script

--- Prolog/eu002.pl
+++ Prolog/eu002.pl
@@ -1,10 +1,16 @@
+:- initialization(search).
+
+search :-
+    search(0, 1, 0, Sum),
+    print(Sum), nl, halt.
+
 search(_, X2, Acc, Acc) :- X2 > 4000000.
 search(X1, X2, Acc, Result) :-
     X3 is X1 + X2,
-    set_new_acc(X3, Acc, NewAcc),
+    sum_even(X3, Acc, NewAcc),
     search(X2, X3, NewAcc, Result).
 
-set_new_acc(X, Acc, NewAcc) :-
+sum_even(X, Acc, NewAcc) :-
     0 is mod(X, 2),
     NewAcc is Acc + X.
-set_new_acc(_, Acc, Acc).
+sum_even(_, Acc, Acc).
