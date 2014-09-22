6eaca31f116f11f02316591f96e3caa613f9d757
Conor McManus <chathurga@gmail.com>
2014-09-22 17:06:06 +0100

    Changed: Euler 2 - Ruby, making fib take a limit seems more natural

--- Ruby/eu002.rb
+++ Ruby/eu002.rb
@@ -1,10 +1,10 @@
-# Set up a generator for all Fibonacci numbers
-def fib
+# Returns a generator for the Fibonacci numbers below a certain limit
+def fib(upto)
   Enumerator.new do |enum|
     x1 = 0
     x2 = 1
     tmp = 0
-    while true
+    while x2 < upto
       enum.yield x2
       tmp = x1 + x2
       x1 = x2
@@ -13,12 +13,8 @@ def fib
   end
 end
 
-# Iterate through the fib numbers until they get bigger than 4 million
 sum = 0
-for x in fib do
-  if x >= 4000000 then
-    break
-  end
+for x in fib(4000000) do
   if x % 2 == 0 then
     sum += x  
   end
