7f9865ec82dab4825a21ef40c4052b2be3f2825f
Conor McManus <chathurga@gmail.com>
2014-09-23 19:53:06 +0100

    Changed: Euler 2 - Clojure, made fib one line

--- Clojure/eu002.clj
+++ Clojure/eu002.clj
@@ -1,4 +1,3 @@
-(defn fib [x1, x2]
-    (cons x2 (lazy-seq (fib x2 (+ x1 x2)))))
+(defn fib [x1, x2] (cons x2 (lazy-seq (fib x2 (+ x1 x2)))))
 
 (println (apply + (filter odd? (take-while #(< % 4000000) (fib 0 1)))))
