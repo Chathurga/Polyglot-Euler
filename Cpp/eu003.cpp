84d919847ec2c882e5c449dedf83cb175e7b511d
Conor McManus <chathurga@gmail.com>
2014-09-24 00:26:00 +0100

    Added: Euler 3 - C++

---
+++ Cpp/eu003.cpp
@@ -0,0 +1,18 @@
+#include<ostream>
+#include<vector>
+
+int main() {
+    long n = 600851475143;
+    std::vector<int> primes({2});
+
+    for (int x = 3; x != n; x += 2) {
+        for(auto prime = primes.begin(); prime != primes.end(); ++prime) {
+            if (x % (*prime) == 0) goto next;
+        }
+        primes.push_back(x);
+        if (n % x == 0) n /= x;
+        next:;
+    }
+
+    printf("%d\n", (int)n);
+}
