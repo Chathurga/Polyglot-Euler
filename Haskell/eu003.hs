14b836eccb031c23c5879684e71dee1697e09980
Conor McManus <chathurga@gmail.com>
2014-09-29 13:26:43 +0100

    Changed: Euler 3 - Haskell, general cleanups

--- Haskell/eu003.hs
+++ Haskell/eu003.hs
@@ -1,17 +1,15 @@
-squareRoot :: Integer -> Integer
-squareRoot = floor . sqrt . fromIntegral
+sqrtInt :: Int -> Int
+sqrtInt = floor . sqrt . fromIntegral
 
-primes :: [Integer]
-primes = 2
-       : [x | x <- [3,5..]
-            , and [x `rem` y /= 0 | y <- takeWhile (<= squareRoot x) primes]]
+primes :: [Int]
+primes = 2 : [x | x <- [3, 5..]
+                , and [x `rem` y /= 0 | y <- takeWhile (<= sqrtInt x) primes]]
 
-
-factorize :: Integer -> [Integer] -> Integer
+factorize :: Int -> [Int] -> Int
 factorize n (p:ps)
   | n == p = p
   | n `rem` p == 0 = factorize (n `quot` p) ps
   | otherwise = factorize n ps
 
-n = 600851475143
-main = print (factorize n primes)
+main :: IO ()
+main = print (factorize 600851475143 primes)
