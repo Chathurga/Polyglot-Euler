9b776c7dcda07be023094c69081631f442aaaf4a
Conor McManus <chathurga@gmail.com>
2014-10-01 18:45:17 +0100

    Changed: Euler 2 - Rust, use 2 spaces for indentation

--- Rust/eu002.rs
+++ Rust/eu002.rs
@@ -3,22 +3,22 @@
 //   take_while, nice for generating infinite series
 
 struct Fib {
-    x1: uint, x2: uint
+  x1: uint, x2: uint
 }
 
 impl Iterator<uint> for Fib {
-    fn next(&mut self) -> Option<uint> {
-        let x3 = self.x1 + self.x2;
-        self.x1 = self.x2;
-        self.x2 = x3;
-        Some(x3)
-    }
+  fn next(&mut self) -> Option<uint> {
+    let x3 = self.x1 + self.x2;
+    self.x1 = self.x2;
+    self.x2 = x3;
+    Some(x3)
+  }
 }
 
 fn main() {
-    let fib = Fib { x1: 0, x2: 1 };
-    let x = fib.take_while(|&x| x < 4000000)
-               .filter(|x| x % 2 == 0)
-               .fold(0u, |a, x| a + x);
-    println!("{}", x);
+  let fib = Fib { x1: 0, x2: 1 };
+  let x = fib.take_while(|&x| x < 4000000)
+             .filter(|x| x % 2 == 0)
+             .fold(0u, |a, x| a + x);
+  println!("{}", x);
 }
