7a89adf9f67de175fe49a071c3f3ed1549802d08
Chathurga <chathurga@gmail.com>
2014-12-22 00:25:53 +0000

    Changed: Euler 2 - Scala

--- Scala/eu002.scala
+++ Scala/eu002.scala
@@ -2,7 +2,7 @@
 
 object Main {
   def main(args: Array[String]) {
-    println(fib(0, 1) takeWhile {_ < 4000000} filter {_ % 2 == 0} sum)
+    println(fib(0, 1).takeWhile(_ < 4000000).filter(_ % 2 == 0).reduce(_+_))
   }
   
   def fib(x1: Int, x2: Int): Stream[Int] = {
